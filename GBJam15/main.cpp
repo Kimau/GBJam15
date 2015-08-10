d47c0201cd273b704642df280f5a40d00f45328e
Claire Blackshaw <evilkimau@gmail.com>
2015-08-10 09:18:31 +0100

    FUCKING STD VECTOR

--- GBJam15/main.cpp
+++ GBJam15/main.cpp
@@ -4,70 +4,69 @@
 #include "tetris.h"
 
 #define SCREEN_TITLE "GBJam #15 - Kimau"
-#define SCREEN_WIDTH  480 // 160
-#define SCREEN_HEIGHT 432 // 144
+#define SCREEN_WIDTH 480   // 160
+#define SCREEN_HEIGHT 432  // 144
 #define GB_WIDTH 160
 #define GB_HEIGHT 144
 
-void Log(const char* msg) {
-	std::cout << "LOG:" << msg << std::endl; // Bleh
+void Log(const char *msg) {
+  std::cout << "LOG:" << msg << std::endl;  // Bleh
 }
 
-void Error(const char* msg) {
-	std::cout << "ERROR:" << msg << " :: " << SDL_GetError() << std::endl; // Bleh
+void Error(const char *msg) {
+  std::cout << "ERROR:" << msg << " :: " << SDL_GetError()
+            << std::endl;  // Bleh
 }
 
-struct SDLAPP
-{
-	SDL_Window* m_window;
-	SDL_Renderer* m_renderer;
+struct SDLAPP {
+  SDL_Window *m_window;
+  SDL_Renderer *m_renderer;
 };
 
-SDLAPP* CreateApp() {
-	if (SDL_Init(SDL_INIT_VIDEO) != 0){
-		Error("SDL_Init");
-		return nullptr;
-	}
+SDLAPP *CreateApp() {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    Error("SDL_Init");
+    return nullptr;
+  }
 
-	SDLAPP* pApp = new SDLAPP();
-	pApp->m_window = SDL_CreateWindow(SCREEN_TITLE, 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if (pApp->m_window == nullptr){
-		Error("SDL_CreateWindow");
-		delete pApp;
-		return nullptr;
-	}
+  SDLAPP *pApp = new SDLAPP();
+  pApp->m_window = SDL_CreateWindow(SCREEN_TITLE, 100, 100, SCREEN_WIDTH,
+                                    SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (pApp->m_window == nullptr) {
+    Error("SDL_CreateWindow");
+    delete pApp;
+    return nullptr;
+  }
 
-	pApp->m_renderer = SDL_CreateRenderer(pApp->m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (pApp->m_renderer == nullptr){
-		Error("SDL_CreateRenderer");
-		SDL_DestroyWindow(pApp->m_window);
-		delete pApp;
-		return nullptr;
-	}	
+  pApp->m_renderer = SDL_CreateRenderer(
+      pApp->m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (pApp->m_renderer == nullptr) {
+    Error("SDL_CreateRenderer");
+    SDL_DestroyWindow(pApp->m_window);
+    delete pApp;
+    return nullptr;
+  }
 
-	return pApp;
+  return pApp;
 }
 
-void CleanQuit(SDLAPP* pApp) {
-	SDL_DestroyRenderer(pApp->m_renderer);
-	SDL_DestroyWindow(pApp->m_window);
-	SDL_Quit();
+void CleanQuit(SDLAPP *pApp) {
+  SDL_DestroyRenderer(pApp->m_renderer);
+  SDL_DestroyWindow(pApp->m_window);
+  SDL_Quit();
 }
 
-void RenderTestScene(uint16_t* pixs, SDL_Rect srcRect)
-{
-	uint16_t GBAColours[4] = {0x141, 0x363, 0x9B1, 0xAC1};
+void RenderTestScene(uint16_t *pixs, SDL_Rect srcRect) {
+  uint16_t GBAColours[4] = {0x141, 0x363, 0x9B1, 0xAC1};
 
-	for (int x = 0; x < GB_WIDTH; x++)
-	{
-		for (int y = 0; y < GB_HEIGHT; y++)
-		{
-			pixs[x+y*GB_WIDTH] = GBAColours[y%4];
-		}
+  for (int x = 0; x < GB_WIDTH; x++) {
+    for (int y = 0; y < GB_HEIGHT; y++) {
+      pixs[x + y * GB_WIDTH] = GBAColours[y % 4];
+    }
 
-		pixs[x] = 0x0FFF;
-		pixs[x+(GB_HEIGHT/2)*GB_WIDTH] = 0x0F00;
-	}
+    pixs[x] = 0x0FFF;
+    pixs[x + (GB_HEIGHT / 2) * GB_WIDTH] = 0x0F00;
+  }
 }
 
 const Uint32 s_FrameRate = 1000 / 30;
@@ -75,79 +74,76 @@ static SDL_Event s_event;
 static Uint32 start_time = SDL_GetTicks();
 
 int GameStep() {
-	start_time = SDL_GetTicks();
+  start_time = SDL_GetTicks();
 
-	// Events
-	while (SDL_PollEvent(&s_event)) {
-		switch (s_event.type) {
-		case SDL_WINDOWEVENT:
-			// if (event.window.windowID == windowID)
-			switch (s_event.window.event) {
-		
-			case SDL_WINDOWEVENT_CLOSE: {
-				Log("Event: Close Window");
-				break;
-										}
-			}
-			break;
+  // Events
+  while (SDL_PollEvent(&s_event)) {
+    switch (s_event.type) {
+      case SDL_WINDOWEVENT:
+        // if (event.window.windowID == windowID)
+        switch (s_event.window.event) {
+          case SDL_WINDOWEVENT_CLOSE: {
+            Log("Event: Close Window");
+            break;
+          }
+        }
+        break;
 
-		case SDL_QUIT:
-			Log("Event: QUIT");
-			return 0;
+      case SDL_QUIT:
+        Log("Event: QUIT");
+        return 0;
 
-		default:
-			Log("Event: ");// %d", s_event.type);  // Event Spam
-		}
+      default:
+        Log("Event: ");  // %d", s_event.type);  // Event Spam
+    }
 
-		// TODO :: Handle Event
-	}
+    // TODO :: Handle Event
+  }
 
-	// Update
-	Tick();
-	
-	return 1;
+  // Update
+  Tick();
+
+  return 1;
 }
 
+int main(int argc, char *argv[]) {
+  SDLAPP *pApp = CreateApp();
+  if (pApp == nullptr) {
+    return -1;
+  }
 
-int main( int argc, char* argv[] )
-{
-	SDLAPP* pApp = CreateApp();
-	if(pApp == nullptr) { return -1; }
+  SDL_Rect srcRect = {0, 0, GB_WIDTH, GB_HEIGHT};
+  SDL_Rect tarRect = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+  uint16_t *pixs = new uint16_t[GB_WIDTH * GB_HEIGHT];
+  SDL_Texture *pBackBuffTex =
+      SDL_CreateTexture(pApp->m_renderer, SDL_PIXELFORMAT_RGB444,
+                        SDL_TEXTUREACCESS_STREAMING, GB_WIDTH, GB_HEIGHT);
 
-	SDL_Rect srcRect = { 0,0, GB_WIDTH, GB_HEIGHT};
-	SDL_Rect tarRect = { 0,0, SCREEN_WIDTH, SCREEN_HEIGHT};
-	uint16_t* pixs = new uint16_t[GB_WIDTH*GB_HEIGHT];
-	SDL_Texture* pBackBuffTex = SDL_CreateTexture(pApp->m_renderer, SDL_PIXELFORMAT_RGB444, SDL_TEXTUREACCESS_STREAMING, GB_WIDTH, GB_HEIGHT);
+  StartGame(GB_WIDTH, GB_HEIGHT);
 
-	StartGame(GB_WIDTH, GB_HEIGHT);
+  if (GameStep() == 0) {
+    CleanQuit(pApp);
+    return 0;
+  }
 
-	if(GameStep() == 0) {
-		CleanQuit(pApp);
-		return 0;
-	}
+  // RenderTestScene(pixs, srcRect);
 
-	//RenderTestScene(pixs, srcRect);
-	
-	do 
-	{
-		Render(pixs, &srcRect);
-		SDL_UpdateTexture(pBackBuffTex, &srcRect, pixs, GB_WIDTH*2);
+  do {
+    Render(pixs, &srcRect);
+    SDL_UpdateTexture(pBackBuffTex, &srcRect, pixs, GB_WIDTH * 2);
 
-		SDL_RenderClear(pApp->m_renderer);
-		SDL_RenderCopy(pApp->m_renderer, pBackBuffTex, &srcRect, &tarRect);
-		SDL_RenderPresent(pApp->m_renderer);
+    SDL_RenderClear(pApp->m_renderer);
+    SDL_RenderCopy(pApp->m_renderer, pBackBuffTex, &srcRect, &tarRect);
+    SDL_RenderPresent(pApp->m_renderer);
 
-		// Sleep
-		if ((s_FrameRate) > (SDL_GetTicks() - start_time)) {
-			SDL_Delay((s_FrameRate) -
-				(SDL_GetTicks() - start_time));  // Yay stable framerate!
-		}
+    // Sleep
+    if ((s_FrameRate) > (SDL_GetTicks() - start_time)) {
+      SDL_Delay((s_FrameRate) -
+                (SDL_GetTicks() - start_time));  // Yay stable framerate!
+    }
 
-		
-	} while (GameStep());
-	
+  } while (GameStep());
 
-	CleanQuit(pApp);
-	return 0;
+  CleanQuit(pApp);
+  return 0;
 }
-
--- GBJam15/tetris.cpp
+++ GBJam15/tetris.cpp
@@ -1,214 +1,225 @@
 #include "tetris.h"
 #include <vector>
 
-struct  
-{
+using namespace std;
+
+class Rect {
+public:
+	int x, y, w, h;
+};
+
+class Pt {
+public:
+  int x, y;
+};
+
+struct {
 	SDL_Rect brickArea;
 	uint16_t width, height;
-	SDL_Surface* sprites;
-	std::vector<SDL_Rect> sprRect;
+	SDL_Surface *sprites;
+	vector<Rect> sprRect;
 } gState;
 
-struct Pt
-{
-	int x,y;
-	Pt(int _x, int _y) : x(_x), y(_y) {}
-};
 
 uint16_t GBAColours[4] = {0x141, 0x363, 0x9B1, 0xAC1};
 
 #ifdef _DEBUG
-#define SETPIX(__x,__y,__c) { SDL_assert(((__x)+(__y)*gState.width >= 0) && ((__x)<gState.width) && ((__y)<gState.height)); pixs[(__x)+(__y)*gState.width]=(__c); }
+#define SETPIX(__x, __y, __c)                                                 \
+  {                                                                           \
+    SDL_assert(((__x) + (__y)*gState.width >= 0) && ((__x) < gState.width) && \
+               ((__y) < gState.height));                                      \
+    pixs[(__x) + (__y)*gState.width] = (__c);                                 \
+  }
 #else
-#define SETPIX(__x,__y,__c) { pixs[(__x)+(__y)*gState.width]=(__c); }
-#endif // _DEBUG
+#define SETPIX(__x, __y, __c) \
+  { pixs[(__x) + (__y)*gState.width] = (__c); }
+#endif  // _DEBUG
 
+void SetupSprites() {
+  SDL_Log(SDL_GetBasePath());
+  gState.sprites = SDL_LoadBMP("sprites.bmp");
+  if (gState.sprites == 0) {
+    SDL_Log(SDL_GetError());
+    return;
+  }
 
+  /**/
+  vector<Pt> corners;
 
-void SetupSprites()
-{
-	SDL_Log(SDL_GetBasePath());
-	gState.sprites = SDL_LoadBMP("sprites.bmp");
-	if(gState.sprites == 0) {
-		SDL_Log(SDL_GetError());
-		return;
-	}
+  uint16_t *pixs = (uint16_t *)gState.sprites->pixels;
+  uint16_t framePix = pixs[0];
+  for (int y = 0; y < gState.height; ++y) {
+    for (int x = 0; x < gState.width; ++x) {
+      if ((pixs[x + y * gState.width] == framePix) &&
+          (pixs[x + 1 + y * gState.width] == framePix) &&
+          (pixs[x + (y + 1) * gState.width] == framePix) &&
+          (pixs[(x + 1) + (y + 1) * gState.width] == 0)) {
+		  corners.push_back({ x, y });
+      }
+    }
+  }
 
-	/**/
-	std::vector<Pt> corners;
+  for (auto p = corners.begin(); p != corners.end(); ++p) {
+    // Build Sheet
+	vector<Rect> currSpr;
 
-	uint16_t* pixs = (uint16_t*)gState.sprites->pixels;
-	uint16_t framePix = pixs[0];
-	for (int y=0; y<gState.height; ++y) {
-		for (int x=0; x<gState.width; ++x)
-		{
-			if( (pixs[x+y*gState.width] == framePix) && 
-				(pixs[x+1+y*gState.width] == framePix) && 
-				(pixs[x+(y+1)*gState.width] == framePix) &&
-				(pixs[(x+1)+(y+1)*gState.width] == 0) ) {
-					corners.push_back(Pt(x,y));
+    // Get Width
+    
+    Rect currRect;
+    currRect.x = p->x + 2;
+    currRect.y = p->y + 2;
+	currRect.w = 0;
+
+    int yoffset = p->y * gState.width;
+    for (int x = p->x + 2; x < gState.sprites->clip_rect.w; ++x) {
+      if ((pixs[x + yoffset] == framePix) &&
+          (pixs[x + yoffset + gState.width] == 0)) {
+		  if (currRect.w == 0)
+			  currRect.x = x;
+		  ++currRect.w;
+      } else if ((pixs[x + yoffset] == 0) &&
+                 (pixs[x + yoffset + gState.width] == 0)) {
+		  if (currRect.w > 0) {
+		  currSpr.push_back(currRect);
+		  currRect.w = 0;
+        }
+      } else {
+        break;
+      }
+    }
+
+    // Get Height
+	currRect.h = 0;
+
+    int xoffset = p->x;
+    for (int y = p->y + 2; y < gState.sprites->clip_rect.h; ++y) {
+      if ((pixs[xoffset + y * gState.width] == framePix) &&
+          (pixs[xoffset + 1 + y * gState.width] == 0)) {
+		  if (currRect.h == 0)
+			  currRect.y = y;
+		  ++currRect.h;
+      } else if ((pixs[xoffset + y * gState.width] == 0) &&
+                 (pixs[xoffset + 1 + y * gState.width] == 0)) {
+		  if (currRect.h > 0) {
+			for (auto r = currSpr.begin(); r != currSpr.end(); ++r)
+			{
+				r->y = currRect.y;
+				r->h = currRect.h;
+
+				gState.sprRect.push_back(*r);
 			}
-		}
-	}
+			currRect.h = 0;          
+        }
+      } else {
+        break;
+      }
+    }
 
-	for (auto p = corners.begin(); p != corners.end(); ++p)
-	{
-		// Build Sheet
-		//gState.sprRect
+	// All Done
+	SDL_Log("[%d:%d] spr count now %d", p->x, p->y, gState.sprRect.size());
+  }
+  /**/
 
-		// Get Width
-		int numWidth = 0;
-		int numPixWide = 1;
-
-		SDL_Rect currRect;
-		currRect.x = p->x+2;
-		currRect.y = p->y+2;
-
-		for (int x=p->x+2; x < gState.sprites->clip_rect.w; ++x)
-		{
-			if( (pixs[x+y*gState.width] == framePix) && 
-				(pixs[x+(y+1)*gState.width] == 0) ) {
-				++numPixWide;
-			} else if( (pixs[x+y*gState.width] == 0) && 
-					(pixs[x+(y+1)*gState.width] == 0) ) {
-				if(numPixWide > 0) {
-					currRect.w = numPixWide;
-					numPixWide = 0;
-					++numWidth;
-					gState.sprRect.push_back(currRect);
-				}
-			} else {
-				break;
-			}
-		}
-
-		// Get Height
-		for (int y=p->x+2; y < gState.sprites->clip_rect.h; ++y)
-		{
-			if( (pixs[x+y*gState.width] == framePix) && 
-				(pixs[x+(y+1)*gState.width] == 0) ) {
-					++numPixWide;
-			} else if( (pixs[x+y*gState.width] == 0) && 
-				(pixs[x+1+y*gState.width] == 0) ) {
-					if(numPixWide > 0) {
-						currRect.w = numPixWide;
-						numPixWide = 0;
-						++numWidth;
-						gState.sprRect.push_back(currRect);
-					}
-			} else {
-				break;
-			}
-		}
-	}
-	/**/
-
-		Uint8 bytesPerPix = gState.sprites->format->BitsPerPixel;
-		SDL_Log("Bytes %d", bytesPerPix);
-	
+  Uint8 bytesPerPix = gState.sprites->format->BitsPerPixel;
+  SDL_Log("Bytes %d", bytesPerPix);
 }
-
-
 
 void StartGame(uint16_t width, uint16_t height) {
-	gState.brickArea.x = 4;
-	gState.brickArea.y = 4;
-	gState.brickArea.w = width / 2;
-	gState.brickArea.h = height - 8;
+  gState.brickArea.x = 4;
+  gState.brickArea.y = 4;
+  gState.brickArea.w = width / 2;
+  gState.brickArea.h = height - 8;
 
-	gState.width = width;
-	gState.height = height;
+  gState.width = width;
+  gState.height = height;
 
-	SetupSprites();
-	return;
+  SetupSprites();
+  return;
 }
 
-void Tick(){
+void Tick() {}
 
+void FillRect(uint16_t *pixs, SDL_Rect *tarRect, uint16_t col) {
+  for (uint16_t x = tarRect->x; x < (tarRect->w + tarRect->x); ++x)
+    for (uint16_t y = tarRect->y; y < (tarRect->h + tarRect->y); ++y)
+      SETPIX(x, y, col);
 }
 
-void FillRect(uint16_t* pixs, SDL_Rect* tarRect, uint16_t col) {
-	for (uint16_t x=tarRect->x; x < (tarRect->w + tarRect->x); ++x)
-		for (uint16_t y=tarRect->y; y < (tarRect->h + tarRect->y); ++y)
-			SETPIX(x,y,col);
+void BorderRect(uint16_t *pixs, SDL_Rect *tarRect, uint16_t col, int inset,
+                int outset) {
+  int tlx, tly, brx, bry;
+  tlx = tarRect->x;
+  tly = tarRect->y;
+  brx = tarRect->x + tarRect->w;
+  bry = tarRect->y + tarRect->h;
+
+  for (int x = (tlx - outset); x < (brx + outset); ++x) {
+    for (int y = (tly - outset); y < (tly + inset); ++y) SETPIX(x, y, col);
+
+    for (int y = (bry - inset); y < (bry + outset); ++y) SETPIX(x, y, col);
+  }
+
+  for (int y = (tly - outset); y < (bry + outset); ++y) {
+    for (int x = (tlx - outset); x < (tlx + inset); ++x) SETPIX(x, y, col);
+
+    for (int x = (brx - inset); x < (brx + outset); ++x) SETPIX(x, y, col);
+  }
 }
 
-void BorderRect(uint16_t* pixs, SDL_Rect* tarRect, uint16_t col, int inset, int outset) {
-	int tlx, tly, brx,bry;
-	tlx = tarRect->x;
-	tly = tarRect->y;
-	brx = tarRect->x + tarRect->w;
-	bry = tarRect->y + tarRect->h;
-	
-	for (int x=(tlx - outset); x < (brx + outset); ++x) {
-		for (int y=(tly -outset); y < (tly + inset); ++y)
-			SETPIX(x,y,col);
+void BezelBoxFilled(uint16_t *pixs, SDL_Rect *tarRect, uint16_t loCol,
+                    uint16_t midCol, uint16_t hiCol) {
+  FillRect(pixs, tarRect, midCol);
 
-		for (int y=(bry -inset); y < (bry + outset); ++y)
-			SETPIX(x,y,col);
-	}
+  int tlx, tly, brx, bry;
+  tlx = tarRect->x;
+  tly = tarRect->y;
+  brx = tarRect->x + tarRect->w;
+  bry = tarRect->y + tarRect->h;
 
-	for (int y=(tly - outset); y < (bry + outset); ++y) {
-		for (int x=(tlx -outset); x < (tlx + inset); ++x)
-			SETPIX(x,y,col);
+  for (int x = tlx; x < brx; ++x) {
+    SETPIX(x, tarRect->y, hiCol);
+  }
 
-		for (int x=(brx -inset); x < (brx + outset); ++x)
-			SETPIX(x,y,col);
-	}
+  for (int y = tly; y < bry; ++y) {
+    SETPIX(tarRect->x + tarRect->w, y, hiCol);
+  }
+
+  for (int x = tlx; x < brx; ++x) {
+    SETPIX(x, tarRect->y + tarRect->h, loCol);
+  }
+
+  for (int y = tly; y < bry; ++y) {
+    SETPIX(tarRect->x, y, loCol);
+  }
 }
 
-void BezelBoxFilled(uint16_t* pixs, SDL_Rect* tarRect, uint16_t loCol, uint16_t midCol, uint16_t hiCol) {
-	FillRect(pixs, tarRect, midCol);
+SDL_Rect ShrinkGrow(SDL_Rect *srcRect, int amount) {
+  SDL_Rect newRect;
 
-	int tlx, tly, brx,bry;
-	tlx = tarRect->x;
-	tly = tarRect->y;
-	brx = tarRect->x + tarRect->w;
-	bry = tarRect->y + tarRect->h;
+  newRect.x = srcRect->x - amount;
+  newRect.y = srcRect->y - amount;
+  newRect.w = srcRect->w + amount;
+  newRect.h = srcRect->h + amount;
 
-	for(int x=tlx; x < brx; ++x) {
-		SETPIX(x,tarRect->y,hiCol);
-	}
-
-	for(int y=tly; y < bry; ++y) {
-		SETPIX(tarRect->x+tarRect->w,y,hiCol);
-	}
-
-	for(int x=tlx; x < brx; ++x) {
-		SETPIX(x,tarRect->y+tarRect->h,loCol);
-	}
-
-	for(int y=tly; y < bry; ++y) {
-		SETPIX(tarRect->x,y,loCol);
-	}
+  return newRect;
 }
 
-SDL_Rect ShrinkGrow(SDL_Rect* srcRect, int amount) {
-	SDL_Rect newRect;
+void Render(uint16_t *pixs, SDL_Rect *srcRect) {
+  // Clear Board
+  FillRect(pixs, srcRect, GBAColours[1]);
 
-	newRect.x = srcRect->x - amount;
-	newRect.y = srcRect->y - amount;
-	newRect.w = srcRect->w + amount;
-	newRect.h = srcRect->h + amount;
+  if (gState.sprites == 0)
+    pixs[0] = 0;
+  else
+    pixs[0] = 0xffff;
+  // FillRect(pixs, &(gState.brickArea), GBAColours[0]);
 
-	return newRect;
-}
+  BezelBoxFilled(pixs, &(gState.brickArea), GBAColours[2], GBAColours[1],
+                 GBAColours[0]);
 
-void Render(uint16_t* pixs, SDL_Rect* srcRect) {
-	// Clear Board
-	FillRect(pixs, srcRect, GBAColours[1]);
-
-	if(gState.sprites == 0)
-		pixs[0] = 0;
-	else
-		pixs[0] =0xffff;
-	//FillRect(pixs, &(gState.brickArea), GBAColours[0]);
-
-	BezelBoxFilled(pixs, &(gState.brickArea), GBAColours[2], GBAColours[1], GBAColours[0]);
-
-	/*
-	SETPIX(gState.brickArea.x, gState.brickArea.y, 0x0F00);
-	SETPIX(gState.brickArea.x-1, gState.brickArea.y-1, 0x000F);
-	BorderRect(pixs, &(gState.brickArea), GBAColours[2], -1, 1);
-	*/
+  /*
+  SETPIX(gState.brickArea.x, gState.brickArea.y, 0x0F00);
+  SETPIX(gState.brickArea.x-1, gState.brickArea.y-1, 0x000F);
+  BorderRect(pixs, &(gState.brickArea), GBAColours[2], -1, 1);
+  */
 }
